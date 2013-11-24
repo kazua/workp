29502906323c920307a89221cace843cdd801711
kazua <loveisover777@gmail.com>
2013-11-24 23:27:59 +0900

    Project Euler Problem 8(PHP)

--- problem008.php
+++ problem008.php
@@ -4,7 +4,7 @@
 
 function problem008() {
     for ($a = 1; $a < 1000; $a++)
-        for ($b = 1; $b < 1000 - $a; $b++)
+        for ($b = $a + 1; $b < 1000 - $a; $b++)
             if (pow($a, 2) + pow($b, 2) == pow(1000 - $a - $b, 2))
                 return ($a * $b * (1000 - $a - $b));
 }
