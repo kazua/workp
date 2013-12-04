52e076edca7f1ae294abf8c6e86e3444607e8469
kazua <loveisover777@gmail.com>
2013-12-04 22:15:11 +0900

    Project Euler Problem 9(PHP)

--- problem009.php
+++ problem009.php
@@ -1,12 +1,12 @@
 <?php
-//http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%208
+//http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%209
 //write kazua
 
-function problem008() {
+function problem009() {
     for ($a = 1; $a < 1000; $a++)
         for ($b = $a + 1; $b < 1000 - $a; $b++)
             if (pow($a, 2) + pow($b, 2) == pow(1000 - $a - $b, 2))
                 return ($a * $b * (1000 - $a - $b));
 }
 
-echo problem008();
+echo problem009();
